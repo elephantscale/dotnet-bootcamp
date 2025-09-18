5091373699c2938071ce697737fd5499d7f99c6e
Rushi Sharma <rushisharma0899@gmail.com>
2025-09-18 16:13:06 -0700

    Update IRepository.cs

--- Day3/Lab3.3-Generic-Classes-Methods/Interfaces/IRepository.cs
+++ Day3/Lab3.3-Generic-Classes-Methods/Interfaces/IRepository.cs
@@ -1,32 +1,4 @@
-// namespace Lab3._3_Generic_Classes_Methods.Interfaces;
 
-// public interface IRepository<T, TKey> where T : class where TKey : IEquatable<TKey>
-// {
-//     // Basic CRUD operations
-//     void Add(T entity);
-//     void AddRange(IEnumerable<T> entities);
-//     T? GetById(TKey id);
-//     IEnumerable<T> GetAll();
-//     void Update(T entity);
-//     void Delete(TKey id);
-//     void Delete(T entity);
-
-//     // Query operations
-//     IEnumerable<T> Find(Func<T, bool> predicate);
-//     T? FirstOrDefault(Func<T, bool> predicate);
-//     bool Exists(Func<T, bool> predicate);
-//     int Count();
-//     int Count(Func<T, bool> predicate);
-
-//     // Batch operations
-//     void DeleteRange(IEnumerable<T> entities);
-//     void DeleteRange(IEnumerable<TKey> ids);
-
-//     // Events
-//     event Action<T>? EntityAdded;
-//     event Action<T>? EntityUpdated;
-//     event Action<T>? EntityDeleted;
-// }
 using Lab3._3_Generic_Classes_Methods.Models;
 
 namespace Lab3._3_Generic_Classes_Methods.Interfaces;
